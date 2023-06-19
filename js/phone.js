42f04fedf933dbbbee758ebe42be8ce942656c51
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2023-06-19 14:09:50 +0600

    some changes added

--- js/phone.js
+++ js/phone.js
@@ -3,10 +3,12 @@ document.getElementById('phone-button-plus-1').addEventListener('click', functio
     const newPhoneFieldNumber = updatingProductQuantity(true, 'phone-input-1')
     updateProductTotalPrice(newPhoneFieldNumber, 'phone-price-1')
 })
+
 document.getElementById('phone-button-minus-1').addEventListener('click', function(){
     const newPhoneFieldNumber = updatingProductQuantity(false, 'phone-input-1')
     updateProductTotalPrice(newPhoneFieldNumber, 'phone-price-1')
 })
+
 document.getElementById('phone-order-button-1').addEventListener('click', function(){
     window.location.href = 'service.html'
 })
