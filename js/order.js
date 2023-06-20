76db1ba4413d973d3de770c7ff3ef298667d0830
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2023-06-20 21:45:32 +0600

    pages added and some functionality added

--- js/order.js
+++ js/order.js
@@ -1,4 +1,12 @@
 //even handler for product order now button
 document.getElementById('order-now-button').addEventListener('click', function(){
-    window.location.href = 'service.html'
+    const productTotalPriceElement = document.getElementById('producs-total-price')
+    const productTotalPriceString = productTotalPriceElement.innerText
+    const productTotalPrice = parseFloat(productTotalPriceString)
+    if(productTotalPrice > 0){
+        window.location.href = 'service.html'
+    }
+    else{
+        alert('Please first select your product!')
+    }
 })
